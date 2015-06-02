e17e164ec64b3f315de869cb490167fab05e9ca6
Ryan Del Bel <rdelbel@gmail.com>
2015-06-01 23:36:31 -0400

    initalize db

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 # from flask.ext.sqlalchemy import SQLAlchemy
+# import os
+
+# app = Flask(__name__)
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# db = SQLAlchemy(app)
+
+# from models import Result
+
+
+# @app.route('/', methods=['GET'])
+# def index():
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run()
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String())
+    result = db.Column(JSON)
+    
+
+    def __init__(self, url, result):
+        self.url = url
+        self.result = result
+       
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
