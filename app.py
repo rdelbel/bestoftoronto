5a490ab01c957dfaa8b03d83342a340ca90479db
Ryan Del Bel <rdelbel@gmail.com>
2015-06-01 23:42:57 -0400

    uncomment app.py

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
-# from flask import Flask, render_template
-# from flask.ext.sqlalchemy import SQLAlchemy
-# import os
-
-# app = Flask(__name__)
-# app.config.from_object(os.environ['APP_SETTINGS'])
-# db = SQLAlchemy(app)
-
-# from models import Result
-
-
-# @app.route('/', methods=['GET'])
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run()
-
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -28,15 +9,9 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
